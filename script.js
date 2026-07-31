3694d72a775e16251db7d56a9928e46482d85aac
moonjimtoken <kaylafrank2019@icloud.com>
2026-07-30 21:13:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+const loader = document.getElementById("loader");
 
+const messages = [
+    "📡 Incoming Transmission...",
+    "🛰️ Scanning orbit...",
+    "👨‍🚀 Crew verified...",
+    "🚀 Launch authorization received...",
+    "🌙 WELCOME, ASTRONAUT."
+];
+
+const text = document.querySelector(".loader-content p");
+
+let i = 0;
+
+const interval = setInterval(() => {
+
+    i++;
+
+    if(i < messages.length){
+
+        text.textContent = messages[i];
+
+    }
+
+},700);
+
+setTimeout(() => {
+
+    clearInterval(interval);
+
+    loader.classList.add("hidden");
+
+},3500);
